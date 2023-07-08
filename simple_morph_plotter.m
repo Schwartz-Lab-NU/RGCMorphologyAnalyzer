7b1ea509eaf513c78f7b477917767fb7f1990ec1
Greg Schwartz <greg.schwartz@northwestern.edu>
2023-07-08 16:53:14 -0500

    added strat plot

--- simple_morph_plotter.m
+++ simple_morph_plotter.m
@@ -1,11 +1,11 @@
 function simple_morph_plotter(appdata)
 figure;
-subplot(2,1,1);
+subplot(3,1,1);
 scatter(appdata.nodes_flattened(:,1),appdata.nodes_flattened(:,2),'ko','filled');
 axis('equal');
 xlabel('X (µm)')
 ylabel('Y (µm)')
-subplot(2,1,2);
+subplot(3,1,2);
 hold('on');
 scatter(appdata.nodes_flattened(:,1),appdata.nodes_flattened(:,3),'ko','filled')
 line([0, max(appdata.nodes_flattened(:,1))],[appdata.lower_surface_z appdata.lower_surface_z],'Color','r','linestyle','--')
@@ -13,4 +13,13 @@ line([0, max(appdata.nodes_flattened(:,1))],[appdata.upper_surface_z appdata.upp
 xlabel('X (µm)')
 ylabel('Depth (µm)')
 ylim([0, appdata.upper_surface_z * 1.5])
+hold('off');
+subplot(3,1,3);
+hold('on');
+plot(appdata.strat_x,appdata.strat_y_norm,'k-','LineWidth',2);
+line([0 0], [0 1], 'Color','r','linestyle','--');
+line([1 1], [0 1], 'Color','r','linestyle','--');
+xlim([-2 1.5]);
+xlabel('IPL depth (normalized)')
+ylabel('Dendrite density (normalized)')
 hold('off');
